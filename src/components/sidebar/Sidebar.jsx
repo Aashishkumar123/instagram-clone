33041e83db03a1df338b9e5f48c4ffde0914f2d5
Aashish <aashishkumar12376@gmail.com>
2023-02-07 11:30:40 +0530

    [FIXED] issue in mode.

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,18 +1,45 @@
 import "./sidebar.css";
 import instagram_black_logo from "../../static/images/instagram-black.png";
 import instagram_white_logo from "../../static/images/instagram-white.png";
+import profile from "../../static/images/profile.png";
 import { Link } from "react-router-dom";
-import { SwitchMode } from "./Switchmode";
+import { useState, useEffect } from "react";
 
 
 export const Sidebar = ()=>{
+
+    var theme = window.localStorage.getItem('theme');
+    if(theme === null){
+        document.body.classList = 'light';
+    }
+    else{
+        var default_mode = theme;
+    }
+
+    const [mode,setMode] = useState(default_mode);
+
+    const ChangeTheme = ()=>{
+        var theme = window.localStorage.getItem('theme');
+        if(theme === 'light'){
+            window.localStorage.setItem('theme','dark');
+            setMode('dark');
+        }
+        else if(theme === 'dark'){
+            window.localStorage.setItem('theme','light');
+            setMode('light');
+        }
+    }
+
+    useEffect(()=>{
+        document.body.classList = mode;
+    })
     return(
         <>
             <div className="instagram-sidebar py-4">
-                <div className="lnsta-logo px-4">
+                <div className="lnsta-logo px-4 pt-3">
                     <img {...(localStorage.getItem('theme') === 'light' ? {src : instagram_black_logo} : {src : instagram_white_logo})} className="img-fluid" alt="instagram-logo" />
                 </div>
-                <div className="insta-menu mt-4">
+                <div className="insta-menu mt-5">
                     <Link to="" className="d-flex align-items-center px-3 py-2 mx-2 mt-3">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14" height="22" width="22"><g><polyline points="0.5 7 7 0.5 13.5 7" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></polyline><polyline points="2.5 8.5 2.5 13.5 11.5 13.5 11.5 8.5" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></polyline></g></svg>
                         <span className="px-3">Home</span>
@@ -42,15 +69,15 @@ export const Sidebar = ()=>{
                         <span className="px-3">Create</span>
                     </Link>
                     <Link to="" className="d-flex align-items-center px-3 py-2 mx-2 mt-3">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14" height="22" width="22"><g><circle cx="7" cy="5.5" r="2.5" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></circle><path d="M10.31,10.75a5,5,0,0,0-6.62,0" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path><path d="M13.5,10.5v2a1,1,0,0,1-1,1h-2" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path><path d="M10.5.5h2a1,1,0,0,1,1,1v2" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path><path d="M.5,3.5v-2a1,1,0,0,1,1-1h2" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path><path d="M3.5,13.5h-2a1,1,0,0,1-1-1v-2" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path></g></svg>
+                        <img src={profile} alt="" style={{width:"25px",borderRadius:"50%"}} />
                         <span className="px-3">Profile</span>
                     </Link>
                     <div className="dropup" style={{position:"absolute",bottom:"35px",width:"16%"}}>
-                        <Link to="" className="d-flex align-items-center px-3 py-2 mx-2" data-bs-toggle="dropdown" aria-expanded="false">
+                        <Link to="" className="d-flex align-items-center px-3 py-2 mx-2" data-bs-toggle="dropdown" aria-expanded="false" id="more-dropdown">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14" height="22" width="22"><g><line x1="13.5" y1="2" x2="0.5" y2="2" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></line><line x1="13.5" y1="7" x2="0.5" y2="7" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></line><line x1="13.5" y1="12" x2="0.5" y2="12" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></line></g></svg>
                             <span className="px-3">More</span>
                         </Link>
-                        <ul class="dropdown-menu mb-2 py-1">
+                        <ul class="dropdown-menu mb-2 py-1 shadow">
                             <li className="d-flex align-items-center py-1">
                                 <a class="dropdown-item" href="#">
                                     Settings
@@ -61,7 +88,10 @@ export const Sidebar = ()=>{
                                 <a class="dropdown-item" href="#">Saved</a>
                                 <svg className="mx-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14" height="25" width="25"><path d="M11,13.5l-4-4-4,4V1.5a1,1,0,0,1,1-1h6a1,1,0,0,1,1,1Z" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path></svg>
                             </li>
-                            <SwitchMode />
+                            <li className="d-flex align-items-center py-1" onClick={ChangeTheme}>
+                                <a class="dropdown-item" href="#">Switch appearance</a>
+                                <svg className="mx-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14" height="25" width="25"><path d="M12,10.48A6.55,6.55,0,0,1,6.46.5a6.55,6.55,0,0,0,1,13A6.46,6.46,0,0,0,13,10.39,6.79,6.79,0,0,1,12,10.48Z" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+                            </li>
                             <li className="d-flex align-items-center py-1">
                                 <a class="dropdown-item" href="#">Your activity</a>
                                 <svg className="mx-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14" height="25" width="25"><g><path d="M13.5,6.92a7.62,7.62,0,0,1-.74,3.21" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path><path d="M7,13.42A6.5,6.5,0,0,1,.5,6.92" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path><polyline points="9 8.92 7 6.42 4 6.42" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></polyline><circle cx="12" cy="11.42" r="1.5" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></circle><path d="M.92,4.52A6.31,6.31,0,0,1,1.79,3" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path><path d="M3.72,1.28A6.3,6.3,0,0,1,5.53.58" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path><path d="M13.08,4.52A6.31,6.31,0,0,0,12.21,3" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path><path d="M10.28,1.28A6.3,6.3,0,0,0,8.47.58" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path></g></svg>
